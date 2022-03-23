b4bdd8590b67cd00c7fdb5e144aba829754d762d
Muhammad Samin Hasan <53146728+saminhasan@users.noreply.github.com>
2022-03-23 20:07:51 +0600

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,88 @@
+#! /bin/python3
+
+import tkinter as tk  
+
+class Application(tk.Frame):
+	def __init__(self, master=None):
+		super().__init__(master)
+		self.master = master
+		#self.master.geometry("320x240")
+		self.pack()
+		self.connection_status = 'disconnected'
+		self.counter = 0
+		self.uplimit = None
+		self.lowlimit = None
+		
+		self.create_widgets()
+		
+	def create_widgets(self):
+		self.button1 = tk.Button(self, text = "Move Up", command = self.moveup)
+		self.button1.grid(sticky="ew", row = 0, column = 0, padx=4, pady=4)
+
+		self.button2 = tk.Button(self, text = "Move Down", command = self.movedown, padx=4, pady=4)
+		self.button2.grid(sticky="ew",row = 1, column = 0)
+
+		self.slider = tk.Scale(self, from_ = 100, to_ = -100, tickinterval=1, command=self.set_position)
+		self.slider.grid(sticky="ew", row = 0, column = 1, rowspan = 2, padx=4, pady=4)
+		self.slider['state'] = 'disabled'
+		#self.slider['state'] = 'normal'
+
+
+		self.button3 = tk.Button(self, text = "Set uppper limit", command = self.set_upper_limit)
+		self.button3.grid(sticky="ew", row = 0, column = 2, padx=4, pady=4)
+
+		self.button4 = tk.Button(self, text = "Set  lower limit", command = self.set_lower_limit)
+		self.button4.grid(sticky="ew", row = 1, column = 2, padx=4, pady=4)
+	
+	def moveup(self):
+		self.counter += 1
+		#print("moveup : ", self.counter)
+		print("counter : ", self.counter, end='\r')
+
+		
+	def movedown(self):
+		self.counter -= 1
+		#print("movedown : ", self.counter)
+		print("counter : ", self.counter, end='\r')
+
+
+	def set_position(self, slider_value):
+		self.counter = int(slider_value)
+		#print("set_position : ", slider_value)
+		print("counter : ", self.counter, end='\r')
+
+		
+		
+	def set_upper_limit(self):
+		self.uplimit = self.counter
+		if self.uplimit is not None and self.lowlimit is not None and int(self.uplimit) > int(self.lowlimit):
+			self.slider['state'] = 'normal'
+			self.slider.config(from_=self.uplimit, to=self.lowlimit)
+		print(" \n set_upper_limit : ", self.uplimit)
+
+
+		
+	def set_lower_limit(self):
+		self.lowlimit = self.counter
+		if self.uplimit is not None and self.lowlimit is not None and int(self.uplimit) > int(self.lowlimit):
+			self.slider['state'] = 'normal'
+			self.slider.config(from_=self.uplimit, to=self.lowlimit)
+		print("\n set_lower_limit : ", self.lowlimit)
+
+		
+def main():
+	root = tk.Tk()
+	root.title("Open-Loop Height Control")
+	#root.resizable(0, 0)
+
+	app = Application(master=root)
+	app.mainloop()
+	'''
+	try:
+		while True:
+			pass
+	except KeyboardInterrupt:
+		print("KeyboardInterrupt")
+	'''
+if __name__ == "__main__":
+	main()
