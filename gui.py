daf3b4fdb2aaf44927d3acffc29f9bb96ddbac77
saminhasan <saminhasan@iut-dhaka.edu>
2022-03-29 09:34:55 +0600

    .

--- gui.py
+++ gui.py
@@ -1,86 +1,132 @@
 #! /bin/python3
 import time
-import tkinter as tk  
+import tkinter as ttk  
 from pySerialTransfer import pySerialTransfer as pt
 import serial.tools.list_ports
-class Application(tk.Frame):
+
+
+class Application(ttk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		#self.master.geometry("320x240")
+		self.master.geometry("480x320")
 		self.pack()
-		ports = list(serial.tools.list_ports.comports())
-		for p in ports:
-			if "Arduino Mega 2560" in p.description:
-				print (p[0] )
-				self.port = p[0]
-				
-				#p.close()
-				
-		time.sleep(1)
 		
-		self.connection_status = 'disconnected'
+				
+				
+		
 		self.counter = 0
 		self.uplimit = None
 		self.lowlimit = None
+		self.connected = False
+		self.port = None
+		self.ports = list(serial.tools.list_ports.comports())
+		for port in self.ports:
+			if "Arduino Mega 2560" in p.description:
+				print (port[0])
+				self.port = port[0]
+		self.ports.append("None")
 		
 		self.create_widgets()
-		#self.port = 'COM12'
-		self.link = pt.SerialTransfer(self.port)
+		#self.link = pt.SerialTransfer(self.port)
 		
 	def create_widgets(self):
-		self.button1 = tk.Button(self, text = "Move Up", command = self.moveup)
-		self.button1.grid(sticky="ew", row = 0, column = 0, padx=4, pady=4)
+	
+		self.button0 = ttk.Button(self, text = "Connect", command = self.connect)
+		self.button0.grid(sticky="ew", row = 0, column = 0, columnspan=3,  padx=4, pady=4)
+		if len(self.ports) ==1:
+			self.button0['state'] = 'disabled'
+		else:
+			self.button0['state'] = 'normal'
+		
+		
+		self.port_number = ttk.StringVar()
+		self.port_number.set(self.port)
+		
+		self.ddmenu = ttk.OptionMenu(self, self.port_number, *self.ports)
+		self.ddmenu.grid(sticky="ew", row = 1, column = 0, columnspan=3,  padx=4, pady=4)	
+		if len(self.ports) == 0:
+			self.ddmenu['state'] = 'disabled'
+		else:
+			self.ddmenu['state'] = 'normal'
 
-		self.button2 = tk.Button(self, text = "Move Down", command = self.movedown, padx=4, pady=4)
-		self.button2.grid(sticky="ew",row = 1, column = 0)
+		
+		self.button1 = ttk.Button(self, text = "Move Up", command = self.moveup)
+		self.button1.grid(sticky="ew", row = 2, column = 0, padx=4, pady=4)
 
-		self.slider = tk.Scale(self, from_ = 100, to_ = -100, tickinterval=1, command=self.set_position)
-		self.slider.grid(sticky="ew", row = 0, column = 1, rowspan = 2, padx=4, pady=4)
+		self.button2 = ttk.Button(self, text = "Move Down", command = self.movedown, padx=4, pady=4)
+		self.button2.grid(sticky="ew",row = 3, column = 0)
+
+		self.slider = ttk.Scale(self, from_ = 100, to_ = -100, tickinterval=0, command=self.slider_set_position)
+		self.slider.grid(sticky="ew", row = 2, column = 1, rowspan = 2, padx=4, pady=4)
 		self.slider['state'] = 'disabled'
 		#self.slider['state'] = 'normal'
 
 
-		self.button3 = tk.Button(self, text = "Set uppper limit", command = self.set_upper_limit)
-		self.button3.grid(sticky="ew", row = 0, column = 2, padx=4, pady=4)
+		self.button3 = ttk.Button(self, text = "Set uppper limit", command = self.set_upper_limit)
+		self.button3.grid(sticky="ew", row = 2, column = 2, padx=4, pady=4)
 
-		self.button4 = tk.Button(self, text = "Set  lower limit", command = self.set_lower_limit)
-		self.button4.grid(sticky="ew", row = 1, column = 2, padx=4, pady=4)
+		self.button4 = ttk.Button(self, text = "Set  lower limit", command = self.set_lower_limit)
+		self.button4.grid(sticky="ew", row = 3, column = 2, padx=4, pady=4)
 	
-		self.inputtxt = tk.Text(self, height = 1, width = 24)
-		#self.inputtxt.insert(tk.END, self.DEVICE_IP)
-		self.inputtxt.grid(sticky="ew",row=2, column=0, columnspan=3, padx=4, pady=4)
-		# Button Creation
-		movebutton = tk.Button(self, text = "Move to Position", command = self.move_to_position)
-		movebutton.grid(sticky="ew", row=3, column=0, columnspan=3, padx=4, pady=4)
+		self.inputtxt = ttk.Text(self, height = 1, width = 24)
+		self.inputtxt.insert(ttk.END , self.counter)
+		self.inputtxt.grid(sticky="ew",row=5, column=0, columnspan=3, padx=4, pady=4)
+		
+		self.movebutton = ttk.Button(self, text = "Move to Position", command = self.move_to_position)
+		self.movebutton.grid(sticky="ew", row=6, column=0, columnspan=3, padx=4, pady=4)	
+		
+		self.stopbutton = ttk.Button(self, text = "    STOP   ", command = self.stop, bg='red')
+		self.stopbutton.grid(sticky="news", row=7, column=0,columnspan=3, padx=4, pady=4)
+		
+		
+	def connect(self):
+		if self.port is not None:
+		
+			try:
+				self.link = pt.SerialTransfer(self.port)
+				if self.link:
+					self.connected = True
+			except Exception as exception:
+				print(exception)
+		else:
+			print("Microcontroller Port Not Found")
+		
 		
 	def move_to_position(self):
 		inp = self.inputtxt.get(1.0, "end-1c")
+		#print(inp)
 		self.counter = int(inp)
 		self.send_data()
+			
+
+
+		
 	def moveup(self):
 		self.counter += 1
+		self.inputtxt.delete('1.0', ttk.END)
+		self.inputtxt.insert(ttk.END , self.counter)
+
 		self.send_data()
 		#print("moveup : ", self.counter)
-		print("counter : ", self.counter, end='\r')
 
 		
 	def movedown(self):
 		self.counter -= 1
+		self.inputtxt.delete('1.0', ttk.END)
+		self.inputtxt.insert(ttk.END , self.counter)
 		self.send_data()
-
 		#print("movedown : ", self.counter)
-		print("counter : ", self.counter, end='\r')
 
 
-	def set_position(self, slider_value):
-		self.counter = int(slider_value)
-		self.send_data()
+	def slider_set_position(self, slider_value):
 
-		#print("set_position : ", slider_value)
-		print("counter : ", self.counter, end='\r')
+			self.counter = int(slider_value)
+			self.inputtxt.delete('1.0', ttk.END)
+			self.inputtxt.insert(ttk.END , self.counter)
+			self.send_data()
+			#print("set_position : ", slider_value)
 
-		
 		
 	def set_upper_limit(self):
 		self.uplimit = self.counter
@@ -100,11 +146,18 @@ class Application(tk.Frame):
 		
 		
 	def send_data(self):
+		print("counter : {}".format(self.counter), end='\r')
+
+		return
 		sendsize = 0;
 		sendsize = self.link.tx_obj(self.counter, start_pos=sendsize)
 		self.link.send(sendsize)
+	
+	def stop(self):
+		print("EMERGERCY STOP TBI")
+		
 def main():
-	root = tk.Tk()
+	root = ttk.Tk()
 	root.title("Open-Loop Height Control")
 	#root.resizable(0, 0)
 
