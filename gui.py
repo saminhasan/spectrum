9832af5e8d3b0800211736d688aa3f9128d41ce7
Muhammad Samin Hasan <53146728+saminhasan@users.noreply.github.com>
2022-03-30 03:24:19 +0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ class Application(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		self.master.geometry("1024x480")
+		self.master.geometry("1024x600")
 		self.pack()
 
 		self.counter = 0
